136e9b55a45fe8e74225c192d1668e2afcd53717
minischetti <domminischetti@gmail.com>
2016-11-27 17:41:41 -0500

    Determine width of nav with calc, Adjust various z-index values, Add shrink/grow animations on transition, Consolidate cursor pointer styles

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
-$('.video-toggle, .video-dim, .video-close').click(function(){
+$('.video-toggle, .video-dim').click(function(){
   $("iframe").toggleClass('open');
-  $(".video-close").toggleClass('open');
   $(".dim").toggleClass('open');
   $(".video-container").toggleClass('open');
   $(".container").toggleClass('shrink');
 });
+
 $('#personalize-toggle').click(function(){
-  $("#home").toggleClass('hidden');
-  $("#personalize").toggleClass('visible');
+  $("#home").toggleClass('shrink');
+  $("#personalize").toggleClass('open');
 });
