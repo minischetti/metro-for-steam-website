1ff91e72946c52c0e776b4c48d674173bcb3cfff
minischetti <domminischetti@gmail.com>
2016-11-30 22:35:57 -0500

    Add split to color values, apply selected color to site

--- js/personalize.js
+++ js/personalize.js
@@ -1,14 +1,15 @@
 $('.color').click(function selectColor(){
   //Store Color as variable
-  color = $('input[name=color]:checked', '.color').val() + ' 255';
+  color = $('input[name=color]:checked', '.color').val().split(",");
 
   //Do we need local storage for this?
   //localStorage.setItem('accentColor',color);
   //console.log(color);
 
   //Output Stylesheet with options
-  outString = '"custom.styles"{colors{Focus="' + color + '"}};'
+  outString = '"custom.styles"{colors{Focus="' + color[0] + '"}};'
 
   //Set up download link
   $('#save').attr('href','data:text/plain;charset=utf-8;base64,' + btoa(outString));
+  $(".important, .action").css('color', color[1]);
 });
