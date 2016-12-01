be50bb9194e92ac24bf801191e3db37c3b213882
minischetti <domminischetti@gmail.com>
2016-11-30 22:04:04 -0500

    Fix accent color JS (it works!)

--- js/personalize.js
+++ js/personalize.js
@@ -1,5 +1,14 @@
 $('.color').click(function selectColor(){
-  color = $('input[name=color]:checked', '.color').val();
-  localStorage.setItem('accentColor',color);
-  console.log(color);
+  //Store Color as variable
+  color = $('input[name=color]:checked', '.color').val() + ' 255';
+
+  //Do we need local storage for this?
+  //localStorage.setItem('accentColor',color);
+  //console.log(color);
+
+  //Output Stylesheet with options
+  outString = '"custom.styles"{colors{Focus="' + color + '"}};'
+
+  //Set up download link
+  $('#save').attr('href','data:text/plain;charset=utf-8;base64,' + btoa(outString));
 });
